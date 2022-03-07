38b654676453e1e05072610a567ec5f524bd9dcc
1212wd <98327099+1212wd@users.noreply.github.com>
2022-03-07 16:07:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+COUNTY LIBRARY SYSTEM
+by dan nalyanya
+feb 2022
+mit licence
+c99 compiler
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu()
+       printf(countylibrary);
+        printf(countylibrary);
+         printf(countylibrary);
+          printf(countylibrary);
+
+
+
+int main()
+{
+    printf(countylibrary);
+    return 0;
+}
